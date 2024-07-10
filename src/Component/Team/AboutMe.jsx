fa162c36a1de48c6b2fa7de2da465ce24532a4f1
Phoe-nix-89 <sonkhoseth074@gmail.com>
2024-07-10 07:47:36 +0530

    Project added

---
+++ src/Component/Team/AboutMe.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AboutMe = () => {
+    return (
+        <div className="flex justify-center">
+            <div className="text-white pt-10 text-3xl">Hello all ! My name is <span className="  focus:text-teal-400">Koustav Seth</span></div>
+        </div>
+    )
+}
+
+export default AboutMe;
